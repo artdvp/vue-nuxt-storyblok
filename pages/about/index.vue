6fddb4e503de12faf7545440d4fdb9ef280bcbff
bonbonpa <sphinspf@gmail.com>
2018-03-22 20:43:41 +0700

    edit : setting up storyblog

---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section id="about-page">
+      <h1>Hi, That's Artdvp</h1>
+      <p>We're a bunch of highly motivated and talented peoplem, Blogging about tech stuff.</p>
+  </section>
+</template>

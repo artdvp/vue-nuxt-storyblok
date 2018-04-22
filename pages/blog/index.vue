a708f959aa89f5d445e06d5deee696c657a7208e
bonbonpa <sphinspf@gmail.com>
2018-04-22 19:45:12 +0700

    move post to blog dir

---
+++ pages/blog/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="post">
+    <PostPreview v-for="post in posts" :key="post.id" 
+    :title="post.title"
+    :excerpt="post.previewText"
+    :thumbnailImage="post.thumbnailUrl"
+    :id="post.id"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Blog/PostPreview";
+
+export default {
+  components: {
+    PostPreview
+  },
+  asyncData(context) {
+    return context.app.$storyapi
+      .get("cdn/stories", {
+        version: "draft",
+        starts_with: "blog/"
+      })
+      .then(res => {
+        console.log(res);
+        return {
+          posts: res.data.stories.map(bp => {
+            return {
+              id: bp.slug,
+              title: bp.content.title,
+              previewText: bp.content.summary,
+              thumbnailUrl: bp.content.thumbnail
+            };
+          })
+        };
+      });
+  }
+};
+</script>
+
+<style scoped>
+#post {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+@media (min-width: 35rem) {
+  #post {
+    flex-direction: row;
+  }
+}
+</style>
